00c4a6067865210f9b8640dbc0feb6cd0ab83120
muhamd sohaib <msohaib980@gmail.com>
2016-05-02 11:59:54 -0700

    add images

--- js/app.js
+++ js/app.js
@@ -9,28 +9,28 @@ app.controller("mainCtrl", function ($scope){
       desc:"cloth",
       price:"50",
       category:"footwear",
-      image:"image"
+      image:"http://www.fivedollartraveller.com/wp-content/uploads/2014/06/Mens-Moab-Mid-Gore-Tex-M.jpg"
     },
     {
       name:"shoes",
       desc:"plastic",
       price:"20",
       category:"footwear",
-      image:"image"
+      image:"http://nsrgtrading.com/wp-content/uploads/2014/09/66.jpg"
     },
     {
       name:"shoes",
       desc:"leather",
       price:"30",
       category:"footwear",
-      image:"image"
+      image:"http://cdn.shopclues.net/images/detailed/12753/RCFS00003_1424802973.jpg"
     },
     {
       name:"sleepers",
       desc:"fiber",
       price:"25",
       category:"footwear",
-      image:"image"
+      image:"https://s-media-cache-ak0.pinimg.com/736x/f3/b1/f4/f3b1f4567ce5456bfd96bf20d355e90e.jpg"
     }
   ];
 
